4446a9d911021d744e56414de0d6126a8b127830
T Goerke <t.goerke@mailbox.org>
2020-07-17 14:12:57 +0200

    Change: Recordings

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 cd ~/Rack
 export LD_LIBRARY_PATH=~/.Rack/libs:$LD_LIBRARY_PATH 
-qjackctl &
 
-jackd -d alsa -dhw:USB & ./Rack
+qjackctl --start &
+
+# get name of audio device plugged into USB
+device=$(aplay -l -q|grep '\[USB Audio\]'  | awk '{FS=":"; print $3}')                                                                                             
+
+#jackd -d alsa -r44100 -p256 -n3 -D -dhw:${device} & ./Rack
+jackd -d alsa -r44100 -p2048 -n3 -D -dhw:${device} & ./Rack
+
 #jackd -d alsa -dhw0 & ./Rack
 #jackd -d alsa -d:"Scarlett 2i2 USB" & ./Rack
