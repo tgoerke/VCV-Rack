d5bc0389e7773d1417ed27fc8ccc06703d2f7c4c
T Goerke <t.goerke@mailbox.org>
2021-01-21 17:14:11 +0100

    Change: Plaits patch

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ device=$(aplay -l -q|grep '\[USB Audio\]'  | awk '{FS=":"; print $3}')
 
 #jackd -d alsa -r44100 -p256 -n3 -D -dhw:${device} & ./Rack
 jackd -d alsa -r44100 -p2048 -n3 -D -dhw:${device} & ./Rack
+#jackd -d alsa -r44100 -p4096 -n3 -D -dhw:${device} & ./Rack
 
 #jackd -d alsa -dhw0 & ./Rack
 #jackd -d alsa -d:"Scarlett 2i2 USB" & ./Rack
