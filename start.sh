be36174184512c387046ba1508bf481d247588a7
T Goerke <t.goerke@mailbox.org>
2022-09-30 17:52:33 +0200

    Change: start.sh for Rack2

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
-cd ~/Rack
+cd ~/Rack2
 test $(hostname) == "tri" && export LD_LIBRARY_PATH=~/.Rack/libs:$LD_LIBRARY_PATH 
 
 qjackctl --start &
 
+# https://github.com/jackaudio/jackaudio.github.com/wiki/WalkThrough_User_PulseOnJack#redirecting-pulseaudio-to-jack
+# listen to browser audio while patching
+pactl load-module module-jack-sink channels=2; pactl load-module module-jack-source channels=2; pacmd set-default-sink jack_out
 
 # get name of audio device plugged into USB
 device=$(aplay -l -q|grep '\[USB Audio\]'  | awk '{FS=":"; print $3}')                                                                                             
